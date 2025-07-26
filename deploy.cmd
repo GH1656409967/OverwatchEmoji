de39ade99f0ee1deb93d7d6461d6bf08ab3a8231
GH1656409967 <1656409967@qq.com>
2025-07-26 17:34:31 +0800

    20250726173430.878000

---
+++ deploy.cmd
@@ -0,0 +1,15 @@
+
+CALL git config --global core.autocrlf false
+CALL git config --global -l
+CALL git init
+CALL git add .
+REM for /f "delims=" %%A in ('wmic os get localdatetime ^ find "."') do set datetime=%%A
+for /f "tokens=2 delims==+" %%A in ('wmic os get localdatetime /value') do set datetime=%%A
+echo %datetime%
+CALL git commit -m "%datetime%"
+CALL git checkout -b main
+CALL git remote add origin git@github.com:GH1656409967/OverwatchEmoji.git
+CALL git push -f --set-upstream origin editor
+REM pause
+
+
